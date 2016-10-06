dd47d2da6e73f01c6fc11e992680b8b7ad392c45
Jeff Chew <jeff.chew@agencydominion.com>
2016-10-06 17:28:30 -0300

    PHP and JavaScript localization

--- js/fsn-base-components-admin.js
+++ js/fsn-base-components-admin.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function() {
 		};	
 		jQuery.post(ajaxurl, data, function(response) {
 			if (response == '-1') {
-				alert(string_translation.error);
+				alert(fsnBaseL10n.error);
 				return false;
 			}
 			listItemsContainer.append(response);
@@ -100,7 +100,7 @@ function fsnBaseInitPostSelect() {
 			minimumInputLength: 1,
 			language: {
 				inputTooShort: function(args) {
-					return string_translation.search;
+					return fsnBaseL10n.search;
 				}
 			}
 		});
