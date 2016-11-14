1dfdaa2451f1988bed0754cb36afb52cb1186bf0
Jeff Chew <jeff.chew@agencydominion.com>
2016-11-14 12:59:36 -0400

    escape get_permalink instances

--- image.php
+++ image.php
@@ -1,7 +1,7 @@
 <?php
 global $post;
 if ( is_attachment() && isset($post->post_parent) && is_numeric($post->post_parent) && ($post->post_parent != 0) ) {
-	wp_redirect(get_permalink($post->post_parent), 301);
+	wp_redirect(esc_url(get_permalink($post->post_parent), 301));
 	exit;
 } elseif ( is_attachment() && isset($post->post_parent) && is_numeric($post->post_parent) && ($post->post_parent < 1) ) {
 	wp_redirect(site_url(), 302);
